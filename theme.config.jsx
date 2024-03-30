d8643e58c2e2b88d15d085161050d3047f200106
vaishnav-mk <vaishnav240204@gmail.com>
2024-03-30 07:49:39 +0530

    head

--- theme.config.jsx
+++ theme.config.jsx
@@ -1,6 +1,24 @@
+import { useRouter } from 'next/router';
+
 export default {
   logo: <span>Sentinel Documentation</span>,
   project: {
     link: "https://github.com/vaibhavraj-exe/sentinel",
   },
+  head: (
+    <>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta property="og:title" content="Sentinel" />
+      <meta property="og:description" content="Sentinel Documentation" />
+    </>
+  ),
+  useNextSeoProps() {
+    const { asPath } = useRouter()
+    if (asPath !== '/') {
+      return {
+        titleTemplate: '%s – Sentinel'
+      }
+    }
+  }
+
 };
